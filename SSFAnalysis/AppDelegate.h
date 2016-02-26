893e96f597db2d1489e2c3c8729200a2bf340771
peter.shi <532289386@qq.com>
2016-02-26 14:28:52 +0800

    Initial Commit

---
+++ SSFAnalysis/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SSFAnalysis
+//
+//  Created by 赛峰 施 on 16/2/26.
+//  Copyright © 2016年 赛峰 施. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SSFAnalysis/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SSFAnalysis
+//
+//  Created by 赛峰 施 on 16/2/26.
+//  Copyright © 2016年 赛峰 施. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
